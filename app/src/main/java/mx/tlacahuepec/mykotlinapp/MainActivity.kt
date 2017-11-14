30c773d364f0d3a4b1cbd21c97b338192587d372
Edward Santiesteban <santiesteban@outlook.com>
2017-11-14 15:43:39 -0600

    Adding sample to using Kotlin Android Extensions

--- app/src/main/java/mx/tlacahuepec/mykotlinapp/MainActivity.kt
+++ app/src/main/java/mx/tlacahuepec/mykotlinapp/MainActivity.kt
@@ -3,10 +3,13 @@ package mx.tlacahuepec.mykotlinapp
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 
+import kotlinx.android.synthetic.main.activity_main.*
+
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        hello.text = getString(R.string.hello_android_extensions)
     }
 }
